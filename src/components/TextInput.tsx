13b149c229e8ee09aeb4992ca63430b8818f919f
Andrii <andriy30534>
2024-03-15 18:59:31 +0200

    feat:added text field

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,49 @@
+import { Box, Button, Stack } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import { useEventStore } from '../stor/eventStore';
+
+export default function TextInput({ dayDate }: { dayDate: string }) {
+  const [text, setText] = useState('');
+  const [inputError, setInputError] = useState('');
+
+  const { createEvent } = useEventStore();
+
+  const submit: React.FormEventHandler<HTMLFormElement> = event => {
+    event.preventDefault();
+    if (text) {
+      createEvent({
+        description: text,
+        event_date: dayDate,
+      });
+      setInputError('');
+
+      setText('');
+    } else {
+      setInputError('field cannot be empty');
+    }
+  };
+  return (
+    <form onSubmit={submit}>
+      <Stack direction='row'>
+        <TextField
+          autoComplete='off'
+          fullWidth
+          value={text}
+          onChange={event => setText(event.target.value)}
+          placeholder='new todo...'
+          error={!!inputError}
+          helperText={inputError}
+        />
+        <Box>
+          <Button
+            sx={{ width: '150px', minHeight: '48px', p: '16px', ml: 1 }}
+            variant='contained'
+            type='submit'>
+            Add
+          </Button>
+        </Box>
+      </Stack>
+    </form>
+  );
+}
---
+++ src/components/form/RegisterForm.tsx
@@ -0,0 +1,83 @@
+import { Box, Button, Dialog, Stack, TextField } from '@mui/material';
+import { useState } from 'react';
+import { IUserRegister } from '../../types';
+
+interface IRegisterForm {
+  openRegister: boolean;
+  isUser: boolean;
+  setOpenRegister: (open: boolean) => void;
+  registerUser: (user: IUserRegister) => void;
+}
+export default function RegisterForm({
+  openRegister,
+  isUser,
+  setOpenRegister,
+  registerUser,
+}: IRegisterForm) {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [inputError, setInputError] = useState('');
+  const handleClose = () => {
+    setOpenRegister(false);
+  };
+  const submit: React.FormEventHandler<HTMLFormElement> = event => {
+    event.preventDefault();
+    if (email && password && name) {
+      registerUser({ name, email, password });
+    } else {
+      setInputError('field cannot be empty');
+    }
+    if (isUser) {
+      setInputError('');
+      setPassword('');
+      setEmail('');
+      setName('');
+      setOpenRegister(false);
+    }
+  };
+  return (
+    <Dialog
+      open={openRegister}
+      maxWidth={'md'}
+      fullWidth={true}
+      onClose={handleClose}>
+      <form onSubmit={submit}>
+        <Stack sx={{ py: 4, px: 4 }} justifyContent={'center'} gap={3}>
+          <TextField
+            autoComplete='off'
+            label='Name'
+            value={name}
+            onChange={event => setName(event.target.value)}
+            error={!!inputError}
+            helperText={inputError}
+          />
+          <TextField
+            autoComplete='off'
+            label='Email'
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+            error={!!inputError}
+            helperText={inputError}
+          />
+          <TextField
+            autoComplete='off'
+            label='Password'
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+            error={!!inputError}
+            helperText={inputError}
+          />
+          <Box>
+            <Button
+              sx={{ width: '150px', minHeight: '48px', p: '16px' }}
+              variant='contained'
+              type='submit'>
+              Login
+            </Button>
+          </Box>
+        </Stack>
+      </form>
+    </Dialog>
+  );
+}
